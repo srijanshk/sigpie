6ce392bcc8a7d6bf0d33d02e0707d2f2bf408bde
Srijan Shakya <srijanshakya977@gmail.com>
2022-12-21 21:21:18 +0545

    config fixes

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,14 +1,14 @@
 import { Dialect } from 'sequelize/types';
 
 export const config = {
-    database: {
-        dialect: 'postgres' as Dialect,
-        host: process.env.DATABASE_HOST || 'localhost',
-        port: +process.env.DATABASE_PORT || 5432,
-        username: process.env.DATABASE_USER || 'postgres',
-        password: process.env.DATABASE_PASSWORD || 'postgres',
-        database: process.env.DATABASE_DATABASE || 'trade_signal',
-        logging: false,
-    },
-    jwtPrivateKey: 'jwtPrivateKey',
+  database: {
+    dialect: 'postgres' as Dialect,
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    logging: false,
+  },
+  jwtPrivateKey: 'jwtPrivateKey',
 };
