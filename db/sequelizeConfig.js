b3f97a8f0443161584be20f8b9365c016c843c2c
Srijan Shakya <srijanshakya977@gmail.com>
2022-12-21 17:51:40 +0545

    Config

--- db/sequelizeConfig.js
+++ db/sequelizeConfig.js
@@ -9,4 +9,13 @@ module.exports = {
     database: process.env.DATABASE_DATABASE || 'trade_signal',
     logging: false,
   },
+  production: {
+    dialect: 'postgres',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: +process.env.DATABASE_PORT || 5432,
+    username: process.env.DATABASE_USER || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'postgres',
+    database: process.env.DATABASE_DATABASE || 'trade_signal',
+    logging: false,
+  },
 };
