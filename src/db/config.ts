6332bb51ca4bd24a4983f9774ac05491422ab408
Srijan Shakya <srijanshakya977@gmail.com>
2022-12-26 22:56:41 +0545

    DB

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,6 +1,16 @@
 import { Dialect } from 'sequelize/types';
 require('dotenv').config();
 
+console.log(
+  {
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+  }
+)
+
 export const config = {
   database: {
     dialect: 'postgres' as Dialect,
